f613245a36c2d8acb8763607f6d586b9f27d5b95
Hiroki Mizumoto <h.mizumoto@freebit.net>
2017-05-31 17:45:35 +0900

    remove plugins that depends on other library

--- lib/tarsier/plugins/tarsier_checker_always.py
+++ lib/tarsier/plugins/tarsier_checker_always.py
@@ -3,5 +3,5 @@ from .tarsier_checker_base import TarsierCheckerPlugin
 
 class TarsierCheckerAlways(TarsierCheckerPlugin):
     def check(self, data) -> [bool, dict]:
-        return True, {"message": "this checker always return true", "matched_event": {}}
+        return True, {"message": "this checker always return true", "matched_event": data}
 
